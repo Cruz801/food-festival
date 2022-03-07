f2bb08b5e4175a1e6cdb004eac13ba2ff449db8d
Steven Cruz <cruz801@hotmail.com>
2022-03-07 12:15:07 -0700

    webpack

---
+++ assets/js/events.js
@@ -0,0 +1,8 @@
+require("bootstrap");
+const createEl = require("./domMethods");
+const createLoremIpsum = require("./helpers");
+
+
+$(document).ready( function() {
+if (window.location.href.indexOf("event") > -1); 
+})
